af00362158a3b15cc24a6df36273ebe8da413993
Bennett Hardwick <bennetthardwick@icloud.com>
2018-09-18 20:43:29 +1000

    PRACTICAL 1

---
+++ load-config.sh
@@ -0,0 +1,3 @@
+sh /home/bennett/config/set-routing.sh
+
+echo "nameserver	1.1.1.1\nnameserver 1.0.0.1" > /etc/resolv.conf
---
+++ set-routing.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Reset iptables config
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -F
+
+# Allow only SSH on enp0s3
+iptables -A INPUT -i enp0s3 -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -i enp0s3 -j DROP
+
+# Configure Static IPs
+ip addr add 10.10.1.1/24 dev enp0s8
+ip link set enp0s8 up
+
+# Configure Routes
+ip route delete default
+ip route add default via 10.10.1.254 dev enp0s8
