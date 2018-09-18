e9578a358e8152ec96301d8773d21326876c7ec2
Bennett Hardwick <bennetthardwick@icloud.com>
2018-09-18 10:49:22 +0000

    PRACTICAL 1

--- load-config.sh
+++ load-config.sh
@@ -1,4 +1,3 @@
 sh /home/bennett/config/set-routing.sh
-sh /home/bennett/config/set-firewall.sh
 
 echo "nameserver	1.1.1.1\nnameserver	1.0.0.1" > /etc/resolv.conf
--- set-routing.sh
+++ set-routing.sh
@@ -5,5 +5,3 @@ ip addr add 192.168.1.254/24 dev enp0s8
 ip link set enp0s8 up
 
 ip route add 10.10.1.0/24 via 192.168.1.80 dev enp0s8
-
-
