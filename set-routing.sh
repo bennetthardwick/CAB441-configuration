6ef8a46c0c904af83b9d61960541fb132497ef44
Bennett Hardwick <bennetthardwick@icloud.com>
2018-10-02 10:56:38 +0000

    Add git ports

--- set-routing.sh
+++ set-routing.sh
@@ -6,8 +6,13 @@ iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -F
 
-# Allow only SSH on enp0s3
+# Allow Port 22 For SSH
 iptables -A INPUT -i enp0s3 -p tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -o enp0s3 -p tcp --sport 22 -j ACCEPT
+iptables -A OUTPUT -o enp0s3 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i enp0s3 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+
+
 iptables -A INPUT -i enp0s3 -j DROP
 
 # Configure Static IPs
@@ -24,5 +29,3 @@ ip route delete default
 
 ip route add default via 192.168.1.254 dev enp0s8
 ip route add 10.10.1.0/24 via 192.168.1.1 dev enp0s8
-
-
